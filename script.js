f10175c12e54e530e26623105145ea8717f70dce
cssmith07060 <cssmith07060@gmail.com>
2022-08-08 18:11:48 -0400

    flow joe

---
+++ script.js
@@ -0,0 +1,8 @@
+const labels = document.querySelectorAll('.form-controllabel')
+
+labels.forEach(label => {
+    .split('')
+    .map((lettter, idx) => `<span>${letter}</span>`)
+    .join('')
+    
+})
