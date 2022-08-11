410639f3185e40a27308edd453b59215225b2465
cssmith07060 <cssmith07060@gmail.com>
2022-08-11 02:36:49 -0400

    lovemeorleavealone

--- script.js
+++
@@ -1,8 +0,0 @@
-const labels = document.querySelectorAll('.form-controllabel')
-
-labels.forEach(label => {
-    .split('')
-    .map((lettter, idx) => `<span>${letter}</span>`)
-    .join('')
-    
-})
